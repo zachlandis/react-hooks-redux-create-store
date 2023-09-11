57e1f80dac8f53fea59e3b6ffa6440674ac7b258
Zach <zachlandis91@gmail.com>
2023-09-11 14:18:37 -0600

    commit tree f8b8a7174a92a5daf37d15913c5d04dc26d50cef

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,20 @@
-let state;
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return { 
+    dispatch,
+    getState
+  }
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +26,15 @@ function reducer(state = { count: 0 }, action) {
   }
 }
 
-function dispatch(action) {
-  state = reducer(state, action);
-  render();
-}
-
 function render() {
   let container = document.getElementById("container");
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 }
 
-dispatch({ type: "@@INIT" });
+let store = createStore(reducer)
+store.dispatch({ type: "@@INIT" });
 let button = document.getElementById("button");
 
 button.addEventListener("click", function () {
-  dispatch({ type: "counter/increment" });
+  store.dispatch({ type: "counter/increment" });
 });
